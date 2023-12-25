f96ce64a2807dd6fa8bdcfec8a310850e94ae56f
3DLaki <94155769+3DLaki@users.noreply.github.com>
2023-12-25 01:33:50 +0100

    Finished product

---
+++ script.js
@@ -0,0 +1,87 @@
+setTimeout(function () {
+    document.getElementById('intro').style.opacity = '1';
+}, 1000);
+setTimeout(function () {
+    document.getElementById('dugme1').style.opacity = '1';
+}, 3000);    
+
+element.addEventListener('click', myFunction);
+
+function toPesmica(){
+    document.getElementById('intro').style.opacity = '0';
+    document.getElementById('intro').style.zIndex = '0';
+                
+    document.getElementById('dugme1').style.opacity = '0';
+    document.getElementById('dugme1').style.zIndex = '0';
+    
+    setTimeout(function () {    
+        document.getElementById('pesmica').style.opacity = '1';
+        document.getElementById('pesmica').style.zIndex = '1';   
+    
+        document.getElementById('dugme21').style.opacity = '1';
+        document.getElementById('dugme21').style.zIndex = '1';  
+    
+        document.getElementById('dugme22').style.opacity = '1';
+        document.getElementById('dugme22').style.zIndex = '1';  
+    }, 300);   
+};
+
+
+function toIntro(){
+    document.getElementById('pesmica').style.opacity = '0';
+    document.getElementById('pesmica').style.zIndex = '0';   
+    
+    document.getElementById('dugme21').style.opacity = '0';
+    document.getElementById('dugme21').style.zIndex = '0';  
+    
+    document.getElementById('dugme22').style.opacity = '0';
+    document.getElementById('dugme22').style.zIndex = '0'; 
+    
+    setTimeout(function () {   
+        document.getElementById('intro').style.opacity = '1';
+        document.getElementById('intro').style.zIndex = '1';
+                
+        document.getElementById('dugme1').style.opacity = '1';
+        document.getElementById('dugme1').style.zIndex = '1';        
+    }, 300);   
+};
+
+
+function toPorukica(){
+    document.getElementById('pesmica').style.opacity = '0';
+    document.getElementById('pesmica').style.zIndex = '0';   
+    
+    document.getElementById('dugme21').style.opacity = '0';
+    document.getElementById('dugme21').style.zIndex = '0'; 
+
+    document.getElementById('dugme22').style.opacity = '0';
+    document.getElementById('dugme22').style.zIndex = '0'; 
+    
+    setTimeout(function () {   
+        document.getElementById('porukica').style.opacity = '1';
+        document.getElementById('porukica').style.zIndex = '1';
+                
+        document.getElementById('dugme3').style.opacity = '1';
+        document.getElementById('dugme3').style.zIndex = '1';        
+    }, 300);   
+};
+
+
+function toPesmicaBack(){
+    document.getElementById('porukica').style.opacity = '0';
+    document.getElementById('porukica').style.zIndex = '0';
+                
+    document.getElementById('dugme3').style.opacity = '0';
+    document.getElementById('dugme3').style.zIndex = '0';
+    
+    setTimeout(function () {    
+        document.getElementById('pesmica').style.opacity = '1';
+        document.getElementById('pesmica').style.zIndex = '1';   
+
+        document.getElementById('dugme21').style.opacity = '1';
+        document.getElementById('dugme21').style.zIndex = '1';  
+        
+        document.getElementById('dugme22').style.opacity = '1';
+        document.getElementById('dugme22').style.zIndex = '1';  
+    }, 300);   
+};
